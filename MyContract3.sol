6de9d8c1b582f369572981788df2f36ddfd21356
David <davidbiagiola5@gmail.com>
2021-05-28 22:30:44 -0400

    2230

--- MyContract3.sol
+++ MyContract3.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.5.1;
+//pragma solidity ^0.5.1;
+pragma experimental ABIEncoderV2; // this allow us to return structs
 
 contract MyContract01 {
     Accounts[] public account;
@@ -7,16 +8,18 @@ contract MyContract01 {
     
     struct Accounts {
         string email;
+        uint position;
     }
     
     function addAccount(
         string memory email  
     ) public {
-        account.push( Accounts(email) );
+        account.push( Accounts(email, peopleCount) );
+        
         peopleCount += 1;
     }
     
-    function findAccount(string memory a) public view returns (string memory) {
+    function findAccount(string memory a) public view returns (Accounts memory) {
         
         // set our initial position
         uint256 position = 0;
@@ -28,10 +31,42 @@ contract MyContract01 {
             if (keccak256(abi.encode(a)) == keccak256(abi.encode(account[position].email)) ) {
                 
                 // save the account found
-                string memory result = account[position].email;
+                //struct  result = { account[position] }
+                
+                /*struct result2 {
+                    string email;
+                    uint position;
+                }*/
+                
+                // end of the loop
+                return account[position];
+            }
+            
+            position += 1;
+        }
+        
+        //return "user not found";
+    }
+    
+    function updateAccount(
+        string memory currentEmail,
+        string memory newEmail
+    ) public payable returns (string memory) {
+        
+        // set our initial position
+        uint256 position = 0;
+        
+        // loop all the values
+        while (position < peopleCount) {
+            
+            // look for the specific email
+            if (keccak256(abi.encode(currentEmail)) == keccak256(abi.encode(account[position].email)) ) {
+                
+                // modify the email 
+                account[position].email = newEmail;
 
                 // end of the loop
-                return result;
+                return "Email was updated";
             }
             
             position += 1;
