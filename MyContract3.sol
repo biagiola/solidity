9bf67e0ab3e4b16a59b3fbda466ba43ff8f82484
David <davidbiagiola5@gmail.com>
2021-05-28 20:08:56 -0400

    2008

--- MyContract3.sol
+++ MyContract3.sol
@@ -1,17 +1,46 @@
 pragma solidity ^0.5.1;
 
-contract MyContract {
-    Person[] public people;
+contract MyContract01 {
+    Accounts[] public account;
+    
     uint256 peopleCount;
     
-    struct Person {
-        string _firstName;
-        string _lastName;
+    struct Accounts {
+        string email;
     }
     
-    function addPerson(string memory _firstName, string memory _lastName) public {
-        people.push(Person(_firstName, _lastName));
+    function addAccount(
+        string memory email  
+    ) public {
+        account.push( Accounts(email) );
         peopleCount += 1;
     }
     
+    function findAccount(string memory a) public view returns (string memory) {
+        
+        // set our initial position
+        uint256 position = 0;
+        
+        // loop all the values
+        while (position < peopleCount) {
+            
+            // look if emails are equals
+            if (keccak256(abi.encode(a)) == keccak256(abi.encode(account[0].email)) ) {
+                
+                // save the account found
+                string memory result = account[position].email;
+
+                // end of the loop
+                return result;
+            }
+            
+            position += 1;
+        }
+    }
 }
+
+// abi.encode() -> convert string into bytes
+
+// bytes(a).length != bytes(people[0]._email).length  -> compare lengths
+
+// constant was removed in version 0.5.0 insted we use "view" or "pure"
