aa4d5df4949cab9e0b870a89d08d1becc62534ef
David <davidbiagiola5@gmail.com>
2021-05-29 15:28:23 -0400

    1528

--- MyContract1.sol
+++ MyContract1.sol
@@ -16,4 +16,30 @@ contract MyContract {
     int public bool1 = ~int256(0); 
 
     int public bool2 = int256(-1);
+
+
+    // 1 - fixed-size types
+    bool public booleans = true;
+    uint public number = 15;
+    address public addressExample = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    bytes32 public data = 0x6162636400000000000000000000000000000000000000000000000000000000; // 64 digits
+
+    // 2 - variable-size types
+    string public name = "david";
+    bytes _dat = 1;
+    uint[] amounts;
+    mapping(uint => string) users;
+    
+    // 3 - user-defined data
+    struct User {
+        uint id;
+        string name;
+        uint[] friendIds;
+    }
+    enum Color {
+        RED,
+        GREE,
+        BLUE
+    }
+
 }
